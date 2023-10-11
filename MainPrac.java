19f8adfa26dcf773fb8a056bf5fcc5e2747e7cc4
hyemin <hyemin.park@mytrum.com>
2023-10-11 21:24:29 +0900

    sorted anwser

--- MainPrac.java
+++ MainPrac.java
@@ -3,14 +3,14 @@ public class MainPrac {
     public static void main(String[] args) {
 
         //int[] nums = {12,345,2,6,7896};
-        int[] nums ={555,901,482,1771};
-        FindNumWithEvenNum sol = new FindNumWithEvenNum();
+        int[] nums ={-4,-1,0,3,10};
 
-        int solution = sol.findNumbers(nums);
+        int[] solution = new int[nums.length];
+        SquaresSortedArray sol = new SquaresSortedArray();
+       
+        solution = sol.sortedSquares(nums);
 
-        int solution2 = sol.findNumbersLimitSolution(nums);
+       System.out.println(solution[0]);
 
-        System.out.println(solution);
-        System.out.println(solution2);
     }
 }
---
+++ SquaresSortedArray.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class SquaresSortedArray {
+
+    /**
+     * Input: nums = [-4,-1,0,3,10]
+        Output: [0,1,9,16,100]
+        Explanation: After squaring, the array becomes [16,1,0,9,100].
+        After sorting, it becomes [0,1,9,16,100].
+        Example 2:
+
+        Input: nums = [-7,-3,2,3,11]
+        Output: [4,9,9,49,121]
+     * 
+     */
+
+    public int[] sortedSquares(int[] nums) {
+    
+        //input값, 제곱한 값 
+        int[] powArr = new int[nums.length];
+        //결과값 
+        //int[] result = new int[nums.length];
+
+        //제곱근
+        for(int i = 0; i< nums.length; i++){
+            powArr[i] = (int) Math.pow(nums[i], 2);
+            System.out.println(powArr[i]);
+        }
+
+        //sort 방식
+        Arrays.sort(powArr);
+
+        //스트림 방식
+        return Arrays.stream(nums)
+                .map(num -> num * num)
+                .sorted()
+                .toArray();
+
+
+    }
+}
