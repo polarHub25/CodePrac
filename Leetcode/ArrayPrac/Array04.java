19b5d58867100dabb8bdaa3ddb8bc699dfa0f544
polarHub25 <olivetree258@gmail.com>
2024-10-01 09:38:24 +0900

    add easy array solution

--- Leetcode/ArrayPrac/Array04.java
+++ Leetcode/ArrayPrac/Array04.java
@@ -8,8 +8,27 @@ public class Array04 {
         int[] nums = {0,0,1,1,1,2,2,3,3,4};
         int sol = array04.removeDuplicates(nums);
 
+        // Comparator<Integer> naturalOrder = (i, j) -> ( i< j) ? -1 : (i == j ? 0 :1);
+        // System.out.println(naturalOrder.compare(new Integer(42), new Integer(42)));
+
+        int[] prices = {7,1,5,3,6,4};
+        int sol2 = array04.maxProfit(prices);
+
     }
     
+    public int maxProfit(int[] prices) {
+     
+        int profit = 0; 
+
+        for(int i = 0; i< prices.length-1; i++){
+            if( prices[i] < prices[i+1]) {
+                profit += prices[i+1] - prices[i];
+            }
+        }
+
+        return profit; 
+    }
+
     public int removeDuplicates(int[] nums) {
 
         if(nums.length == 0) return 0;
