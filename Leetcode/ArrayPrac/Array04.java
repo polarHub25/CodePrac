1518ebc9e900a7c2c45739962a35e518bc0b7509
polarHub25 <olivetree258@gmail.com>
2024-10-04 10:00:09 +0900

    add ratate array solution

--- Leetcode/ArrayPrac/Array04.java
+++ Leetcode/ArrayPrac/Array04.java
@@ -5,17 +5,42 @@ public class Array04 {
         
         Array04 array04 = new Array04();
 
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
-        int sol = array04.removeDuplicates(nums);
+        // int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        // int sol = array04.removeDuplicates(nums);
 
-        // Comparator<Integer> naturalOrder = (i, j) -> ( i< j) ? -1 : (i == j ? 0 :1);
-        // System.out.println(naturalOrder.compare(new Integer(42), new Integer(42)));
+        // int[] prices = {7,1,5,3,6,4};
+        // int sol2 = array04.maxProfit(prices);
 
-        int[] prices = {7,1,5,3,6,4};
-        int sol2 = array04.maxProfit(prices);
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 3;
 
     }
     
+    public void rotate(int[] nums, int k) {
+     
+        /** Input: nums = [1,2,3,4,5,6,7], k = 3
+            Output: [5,6,7,1,2,3,4] */
+        
+        int n = nums.length;
+        k = k % n; 
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+
+    }
+
+    private void reverse(int[] nums , int start, int end){
+
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+
+        }
+    }
+
     public int maxProfit(int[] prices) {
      
         int profit = 0; 
