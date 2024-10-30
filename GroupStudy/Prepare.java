afc1b35272704b0da6805389d9af43f15b8d4d08
polarHub25 <olivetree258@gmail.com>
2024-10-30 16:07:24 +0900

    Longest Palindromic Substring

--- GroupStudy/Prepare.java
+++ GroupStudy/Prepare.java
@@ -6,13 +6,48 @@ public class Prepare {
 
     public static void main(String[] args) {
         
-        String input = "pwwkew";
+        // String input = "pwwkew";
 
-        Prepare pre = new Prepare();
-        int output = pre.longestSubstring(input);
+         Prepare pre = new Prepare();
+        // int output = pre.longestSubstring(input);
 
+        // System.out.println("output :: " + output);
+
+        String s = "babad";
+        
+        String output = pre.longestPalindrome(s);
         System.out.println("output :: " + output);
 
+    }
+
+    //Longest Palindromic Substring
+    public String longestPalindrome(String s) {
+        
+        if(s == null || s.length() < 1) return "";
+
+        int start = 0;
+        int end = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            int odd = calculateRange(s , i , i); // 홀수
+            int even = calculateRange(s , i , i +1 ); // 짝수
+            int len = Math.max(odd, even);
+
+            if( len > end - start){
+                start = i - (len -1)/2;
+                end = i + len /2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+    
+
+    private int calculateRange(String s, int left, int right) {
+        while( left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+        return right - left - 1;
     }
 
     public int longestSubstring(String input){
