736ef6df1d252dd18f260bb2c39140fffa8d55eb
polarHub25 <olivetree258@gmail.com>
2024-11-14 10:21:41 +0900

    add Container With Most Water

--- GroupStudy/Prepare.java
+++ GroupStudy/Prepare.java
@@ -10,11 +10,39 @@ public class Prepare {
         
         Prepare pre = new Prepare();
 
-        String s = "w 4193 with words";
+        int[] height = {1,8,6,2,5,4,8,3,7};;
         
-        int output = pre.myAtoi(s);
+        int output = pre.maxArea(height);
         System.out.println("output :: " + output);
 
+    }
+
+
+    // 11. Container With Most Water
+    //https://leetcode.com/problems/container-with-most-water/description/
+    public int maxArea(int[] height) {
+       // 시간 복잡도 : O(n) 배열을 한번 순회 
+       // 공간 복잡도 : O(1) 추가적인 메모리 사용없이 상수 공간만 사용 
+       
+        
+        int left = 0;
+        int right = height.length -1;
+        int maxArea = 0;
+
+        while(left < right ){
+            int width = right - left;
+            int area = Math.min(height[right], height[left]) * width;
+            maxArea = Math.max(maxArea, area);
+
+            if(height[left] < height[right]){
+                left++;
+            } else {
+                right--;
+            }
+
+        }
+
+        return maxArea;
     }
 
 
